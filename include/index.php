fa022f7a35422ec32cba3751722765c902d1b027
Yuz <wwowkdrr@gmail.com>
2025-05-19 14:37:00 +0800

    使用file_exist及 ?? 將isset()修改簡化

--- include/index.php
+++ include/index.php
@@ -17,7 +17,7 @@
         //測試require / include執行出來的差異、錯誤後的差異
         //include_once = 有優先級
 
-        $page=isset($_GET['page']) ? $_GET['page'] : 'main';
+        $page=$_GET['page'] ?? 'main';
         $file=$page.".php";
         if(file_exists($file)){
             include $file;
