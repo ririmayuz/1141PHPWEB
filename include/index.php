c5ed13189b56a4dce88f11160e9110fc75ea0209
Yuz <wwowkdrr@gmail.com>
2025-05-19 15:37:43 +0800

    利用include 來拓展變數的應用

--- include/index.php
+++ include/index.php
@@ -1,10 +1,31 @@
+<?php $page=$_GET['page'] ?? 'main'; ?> 
+<!-- 更改程式碼順序/調整拿到值的時間點 -->
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>學生管理系統</title>
+    <title>學生管理系統
+    <?php
+    switch($page){
+            case 'list':
+                echo " - 學生列表";
+                break;
+            case 'new':
+                echo " - 新增學生";
+                break;
+            case 'query':
+                echo " - 查詢學生";
+                break;
+            case 'about':
+                echo " - 關於我們";
+                break;
+            default:
+                echo " - 首頁";
+        }
+    ?>
+    </title>
     <link rel="stylesheet" href="style.css">
 </head>
 
@@ -17,7 +38,7 @@
         //測試require / include執行出來的差異、錯誤後的差異
         //include_once = 有優先級
 
-        $page=$_GET['page'] ?? 'main';
+        echo $page;
         $file=$page.".php";
         if(file_exists($file)){
             include $file;
