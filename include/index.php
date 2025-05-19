b65caafa24b4579cec25b31631f3b8d7d428845b
Yuz <wwowkdrr@gmail.com>
2025-05-19 14:21:19 +0800

    使用get及switch case的方式來動態載入頁面

--- include/index.php
+++ include/index.php
@@ -5,66 +5,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>學生管理系統</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-            background-color: #f4f4f4;
-        }
-
-        header {
-            background-color: #333;
-            color: #fff;
-            padding: 10px 0;
-            text-align: center;
-        }
-
-        nav {
-            background-color: #444;
-            color: #fff;
-            padding: 10px 0;
-        }
-
-        nav ul {
-            list-style-type: none;
-            padding: 0;
-            text-align: center;
-        }
-
-        nav ul li {
-            display: inline;
-            margin-right: 20px;
-        }
-
-        nav ul li a {
-            color: #fff;
-            text-decoration: none;
-        }
-
-        main {
-            padding: 20px;
-        }
-
-        footer {
-            background-color: #333;
-            color: #fff;
-            text-align: center;
-            padding: 10px 0;
-            position: fixed;
-            width: 100%;
-            bottom: 0;
-        }
-    </style>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
+    <?php
+        // for($i=0;$i<5;$i++){
+        //     include_once "header.php";
+        // }
+        //測試require / include執行出來的差異、錯誤後的差異
+        //include_once = 有優先級
 
-    <?php include "header.php" ?>
-    <main>
-        <h2>歡迎來到學生管理系統</h2>
-        <p>請從上方選單選擇功能。</p>
-    </main>
+        $page=isset($_GET['page']) ? $_GET['page'] : 'main';
+        switch($page){
+            case 'list':
+                include "list.php";
+                break;
+            case 'new':
+                include "new.php";
+                break;
+            case 'query':
+                include "query.php";
+                break;
+            case 'about':
+                include "about.php";
+                break;
+            default:
+                include "main.php";
+        }
+
+    ?>
     <?php include "footer.php" ?>
 </body>
 
